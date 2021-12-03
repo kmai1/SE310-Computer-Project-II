4e259ae2204b118717bdcac8baec7812063f52b3
kmai1 <maikevin123@gmail.com>
2021-12-03 14:59:55 -0600

    done

--- SE310Project2.py
+++ SE310Project2.py
@@ -6,14 +6,15 @@ import matplotlib.animation as animation
 
 
 #################Definition of the Four Bar Linkage
-#The values are placeholder. Please change the values below. 
+#The values are placeholder. Please change the values below.
 #Please leave theta4=0 and N=360
 
-a=20.0               # The crank
-b=50.0               # The coupler
-c=40.0               # The rocker
-d=40.0              # The fixed link
-e=70.0               # The wing
+a=20.0               # The crank, > 10
+b=60.0               # The coupler
+c=50.0               # The rocker
+d=50.0              # The fixed link
+e=100.0               # The wing
+
 theta4=0            #The orientation of the fixed link
 N=360               # Number of simulation points
 
@@ -31,7 +32,7 @@ if a+max(link_length)>b+c+d-max(link_length):
     print("Grashoff criterion is not satisfied")
 else:
     print("Grashoff criterion is satisfied")
-    
+
 #initializing x and y coordinates of all the links
 x1=np.zeros((t1.shape[0]))
 x2=np.zeros((t1.shape[0]))
@@ -74,28 +75,28 @@ def vfunc(x,a,b,c,d,theta,x1,x2,w1):
     return (re,im)
 
 w=900*2*np.pi/60 #input velocity of the crank
-y0=[0,0]   
+y0=[0,0]
 i=0;fr=0;
 for theta1 in t1: #for the range of input crank equations
     if i>1:
         x0=[t2[i-1],t3[i-1]]
         #theta0=[0,0]#theta2 and theta3 initial guesses are assigned to the previous iteration
     else:
-        x0=[np.pi/2,np.pi]    
+        x0=[np.pi/2,np.pi]
     sol=fsolve(func,x0,args=(a,b,c,d,theta1),full_output=True) #nonlinear solver that solves Eq1 and Eq2
     exit_flag=sol[2] #if exit_flag==1, then the solution has reached and the algorithm is successful
     theta2=sol[0][0]
     theta3=sol[0][1]
     t2[i]=theta2
     t3[i]=theta3
-    
+
     #Finding the tip velocity
     sol2=fsolve(vfunc,y0,args=(a,b,c,d,theta1,theta2,theta3,w),full_output=True) #nonlinear solver that solves Eq1 and Eq2
     exit_flag2=sol2[2] #if exit_flag==1, then the solution has reached and the algorithm is successful
     v1=sol2[0][0]
     v2=sol2[0][1]
     v_tip[i]=abs(v2*e)
-    
+
     if exit_flag==1: #evaluating the x and y coordinates of the solved problem
         x1[fr]=ox
         y1[fr]=oy
@@ -110,15 +111,15 @@ for theta1 in t1: #for the range of input crank equations
         x6[fr]=x4[fr]-e*np.cos(theta3)
         y6[fr]=y4[fr]-e*np.sin(theta3)
 
-        
+
         fr=fr+1
        # plt.plot([x1,x2,x3,x4,x1],[y1,y2,y3,y4,y1])
     i=i+1
-    
+
     if i==1:
-        
+
         line, = ax.plot([x1[fr],x2[fr],x3[fr],x5[fr],x6[fr],x4[fr],x1[fr]], [y1[fr],y2[fr],y3[fr],y5[fr],y6[fr],y4[fr],y1[fr]], 'r')
-        
+
  # Range of angular motion of the wing
 theta3_range = (max(t3) - min(t3))*180/np.pi
 print('range = ')
@@ -130,30 +131,30 @@ tip_vel=max(v_tip)
 print('tip_vel=')
 print(tip_vel)
 
-        
 
 
-    
+
+
 def animation_frame(p):
-   
+
     line.set_data([x1[p],x2[p],x3[p],x5[p],x6[p],x4[p],x1[p]],[y1[p],y2[p],y3[p],y5[p],y6[p],y4[p],y1[p]])
-   
+
     return line
-    
-    
+
+
 ani = animation.FuncAnimation(fig, func=animation_frame, init_func=plot_initialize, frames=np.arange(0,fr),interval=100,repeat=True)
 
-#plt.show()
+plt.show() #uncommented this
 
 
 
 
-    
-    
-    
 
 
-    
+
+
+
+
 
 
 
@@ -170,4 +171,3 @@ ani = animation.FuncAnimation(fig, func=animation_frame, init_func=plot_initiali
 #VV=4.
 #guess = 9
 #sol=fsolve(func, guess, args=(VV),full_output=True)
-
